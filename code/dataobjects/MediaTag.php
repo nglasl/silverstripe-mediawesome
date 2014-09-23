140efd0ce861bc7abfdff94f6e63faeb70fded43
Nathan Glasl <nathan@silverstripe.com.au>
2014-09-23 17:14:32 +1000

    Minor syntax update.

--- code/dataobjects/MediaTag.php
+++ code/dataobjects/MediaTag.php
@@ -70,11 +70,7 @@ class MediaTag extends DataObject {
 		// Confirm that the current tag has been given a title and doesn't already exist.
 
 		$this->Title = strtolower($this->Title);
-		!$this->Title ?
-			$result->error('Title required!') :
-			MediaTag::get_one('MediaTag', "ID != " . intval($this->ID) . " AND Title = '" . Convert::raw2sql($this->Title) . "'") ?
-				$result->error('Tag already exists!') :
-				$result->valid();
+		!$this->Title ? $result->error('Title required!') : (MediaTag::get_one('MediaTag', "ID != " . intval($this->ID) . " AND Title = '" . Convert::raw2sql($this->Title) . "'") ? $result->error('Tag already exists!') : $result->valid());
 
 		// Allow extension customisation.
 
