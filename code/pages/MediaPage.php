71b8249419e52ab937982842cc65486931b504a8
Jake Bentvelzen <jake@silverstripe.com.au>
2016-05-03 17:39:19 +1000

    remove(FrontendCreate): Removed FrontendCreate-specific code

--- code/pages/MediaPage.php
+++ code/pages/MediaPage.php
@@ -77,15 +77,6 @@ class MediaPage extends SiteTree {
 	);
 
 	/**
-	 * The default fields to show for getFrontendCreateFields.
-	 */
-	private static $frontend_create_fields = array(
-		'Title',
-		'Content',
-		'MediaTypeID',
-	);
-
-	/**
 	 *	Apply custom default media types with respective attributes, or additional attributes to existing default media types.
 	 *
 	 *	@parameter <{MEDIA_TYPES_AND_ATTRIBUTES}> array(array(string))
@@ -290,100 +281,6 @@ class MediaPage extends SiteTree {
 				$fields->push($attributeField);
 			}
 		}
-	}
-
-	/**
-	 * Get the frontend fields to use with ObjectCreatorPage type
-	 * (frontend-objects module)
-	 *
-	 * @return FieldList
-	 */
-	public function getFrontendCreateFields() {
-		$fields = $this->scaffoldFormFields();
-		$whitelist = ArrayLib::valuekey($this->stat('frontend_create_fields'));
-		foreach ($fields as $field) {
-			if (!isset($whitelist[$field->getName()])) {
-				$fields->remove($field);
-			}
-		}
-
-		// Update Media Type dropdown to only use available media types
-		$mediaTypeField = $fields->dataFieldByName('MediaTypeID');
-		if ($mediaTypeField && $mediaTypeField instanceof DropdownField) 
-		{
-			$mediaTypeField->setSource($this->getFrontEndCreateMediaTypes());
-			$mediaTypeField->setHasEmptyDefault(false); // Remove blank option, force user to select one.
-
-			// The display logic module requires jQuery, however sometimes people prefer
-			// to roll their own jQuery.js file from their theme, so only use the framework
-			// provided jQuery if they HAVE NOT included jquery.
-			$hasjQuery = false;
-			foreach (Requirements::backend()->get_javascript() as $filename)
-			{
-				if (strpos($filename, 'jquery') !== false) 
-				{
-					$hasjQuery = true;
-					break;
-				}
-			}
-			if (!$hasjQuery)
-			{
-				Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
-			}
-		}
-
-		// Add attribute fields
-		$this->updateCMSAttributeFields($fields);
-
-		return $fields;
-	}
-
-	/**
-	 * Fields to show the user in the review process, removes any attribute
-	 * fields that don't match with the current set MediaType ID.
-	 * (frontend-objects module)
-	 *
-	 * @return FieldList
-	 */
-	public function getFrontendCreateReviewFields() {
-		$fields = $this->getFrontendCreateFields();
-		foreach ($fields as $field)
-		{
-			$attribute = $field->AttributeRecord;
-			if ($attribute && $attribute->MediaTypeID != $this->MediaTypeID) 
-			{
-				$fields->remove($field);
-			}
-		}
-		return $fields;
-	}
-
-	/**
-	 * Get validator for frontend fields used for ObjectCreatorPage.
-	 * (frontend-objects module)
-	 *
-	 * @return MediaPageFrontendCreateValidator
-	 */
-	public function getFrontendCreateValidator() {
-		return MediaPageFrontendCreateValidator::create();
-	}
-
-	/**
-	 * Only gets Media Types that have been set on MediaHolder pages
-	 *
-	 * @return array
-	 */
-	public function getFrontEndCreateMediaTypes(FieldList $fields = null) {
-		$mediaTypeIDs = array();
-		foreach (MediaHolder::get() as $holderPage)
-		{
-			if ($holderPage->MediaTypeID && !isset($mediaTypeIDs[$holderPage->MediaTypeID]) && ($mediaType = $holderPage->MediaType())) 
-			{
-				$mediaTypeIDs[$holderPage->MediaTypeID] = $mediaType->Title;
-			}
-		}
-		// MAYBETODO(Jake): Add ->extend for modifying the media types
-		return $mediaTypeIDs;
 	}
 
 	/**
@@ -700,13 +597,4 @@ class MediaPage_Controller extends Page_Controller {
 		return $this->renderWith($templates);
 	}
 
-}
-
-class MediaPageFrontendCreateValidator extends RequiredFields {
-	public function php($data) {
-		$result = parent::php($data);
-		// maybetodo(jake): If Attribute is a required field, validate here. Need to think about how
-		// 			   to make attributes required first. (extend MediaAttribute?)
-		return $result;
-	}
 }
